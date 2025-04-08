205b4878a34b86277324bcc3aeec01dfbd01fec0
Parikshit Prasai <parikshit.prasai@terahs.ai>
2025-04-08 19:58:26 +0545

    refactor to use the service layer

--- src/App.tsx
+++ src/App.tsx
@@ -1,106 +1,80 @@
+// App.tsx
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import cornerstoneDICOMImageLoader from '@cornerstonejs/dicom-image-loader';
-import { init, RenderingEngine, Enums, Types } from '@cornerstonejs/core';
+import { Enums } from '@cornerstonejs/core';
+import { StackScrollTool, Enums as ToolEnums } from '@cornerstonejs/tools';
+import { LengthTool } from '@cornerstonejs/tools';
+import { DicomQueryParams, fetchImageIds } from './services/dicomService';
 import {
-    Enums as ToolEnums,
-    addTool,
-    StackScrollTool,
-    ToolGroupManager,
-    LengthTool,
-    init as toolsInit,
-} from '@cornerstonejs/tools';
-
-import { fetchImageIds } from './services/fetchImageIds';
+    CornerstoneService,
+    ToolConfiguration,
+    ViewerConfig,
+} from './services/cornerstoneService';
 
 const { ViewportType } = Enums;
 const { MouseBindings } = ToolEnums;
 
-function App(): React.JSX.Element {
-    const [renderingEngine, setRenderingEngine] = useState<RenderingEngine>();
-    const [cornerstoneInitialized, setCornerstoneInitialized] = useState(false);
-    const [imageIds, setImageIds] = useState<string[]>([]);
+const dicomParams: DicomQueryParams = {
+    StudyInstanceUID:
+        '1.3.6.1.4.1.14519.5.2.1.7009.2403.334240657131972136850343327463',
+    SeriesInstanceUID:
+        '1.3.6.1.4.1.14519.5.2.1.7009.2403.226151125820845824875394858561',
+    wadoRsRoot: 'https://d14fa38qiwhyfd.cloudfront.net/dicomweb',
+};
 
-    const render = async (
-        imageIds: string[],
-        viewport: Types.IStackViewport,
-    ) => {
-        await viewport.setStack(imageIds, 10);
-        viewport.render();
-    };
+function App(): React.JSX.Element {
+    const [manager, setManager] = useState<CornerstoneService | null>(null);
+    const [imageIds, setImageIds] = useState<string[]>([]);
+    const [initialized, setInitialized] = useState(false);
 
     useEffect(() => {
-        if (cornerstoneInitialized) return;
-
-        cornerstoneDICOMImageLoader.init();
-        init();
-        toolsInit();
-
-        fetchImageIds({
-            StudyInstanceUID:
-                '1.3.6.1.4.1.14519.5.2.1.7009.2403.334240657131972136850343327463',
-            SeriesInstanceUID:
-                '1.3.6.1.4.1.14519.5.2.1.7009.2403.226151125820845824875394858561',
-            wadoRsRoot: 'https://d14fa38qiwhyfd.cloudfront.net/dicomweb',
-        }).then((ids) => {
+        async function initialize() {
+            const ids = await fetchImageIds(dicomParams);
+            const cornerstoneService = new CornerstoneService();
+            cornerstoneService.createRenderingEngine();
             setImageIds(ids);
-            setCornerstoneInitialized(true);
-            setRenderingEngine(new RenderingEngine('demo-engine'));
-        });
+            setManager(cornerstoneService);
+            setInitialized(true);
+        }
+        initialize();
     }, []);
 
     useEffect(() => {
-        if (!cornerstoneInitialized || imageIds.length <= 0 || !renderingEngine)
-            return;
+        if (!initialized || !manager || imageIds.length === 0) return;
 
         const element = document.getElementById(
             'stack-viewport',
         ) as HTMLDivElement;
+        if (!element) return;
 
-        const viewportInput = {
-            viewportId: 'demo-stack-viewport',
+        const viewerConfig: ViewerConfig = {
+            viewportId: 'stack-viewport',
             element,
-            type: ViewportType.STACK,
-            defaultOptions: {
-                background: [0.2, 0, 0.2] as Types.Point3,
-            },
+            viewerType: ViewportType.STACK,
+            imageIds,
+            defaultOptions: { background: [0.1, 0.1, 0.3] },
+            tools: [
+                {
+                    tool: StackScrollTool,
+                    toolName: StackScrollTool.toolName,
+                    active: true,
+                    bindings: [{ mouseButton: MouseBindings.Wheel }],
+                },
+                {
+                    tool: LengthTool,
+                    toolName: LengthTool.toolName,
+                    active: true,
+                    bindings: [{ mouseButton: MouseBindings.Primary }],
+                },
+            ] as ToolConfiguration[],
         };
 
-        renderingEngine.enableElement(viewportInput);
-
-        const viewport = renderingEngine.getViewport(
-            viewportInput.viewportId,
-        ) as Types.IStackViewport;
-
-        addTool(StackScrollTool);
-        addTool(LengthTool);
-
-        const toolGroup = ToolGroupManager.createToolGroup('stack-tool-group');
-        toolGroup?.addViewport(viewport.id, renderingEngine.id);
-        toolGroup?.addTool(StackScrollTool.toolName);
-        toolGroup?.setToolActive(StackScrollTool.toolName, {
-            bindings: [
-                {
-                    mouseButton: MouseBindings.Wheel,
-                },
-            ],
-        });
-
-        toolGroup?.addTool(LengthTool.toolName);
-        toolGroup?.setToolActive(LengthTool.toolName, {
-            bindings: [
-                {
-                    mouseButton: MouseBindings.Primary,
-                },
-            ],
-        });
-
-        render(imageIds, viewport);
-    }, [cornerstoneInitialized, imageIds, renderingEngine]);
+        manager.setupViewer(viewerConfig);
+    }, [initialized, manager, imageIds]);
 
     return (
         <>
-            {cornerstoneInitialized ? (
+            {initialized ? (
                 <div id="stack-viewport" className="h-[800px] w-[800px]"></div>
             ) : (
                 <p>Loading...</p>
